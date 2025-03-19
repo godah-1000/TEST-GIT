f5f7e9d525ba357ef31ad2b336cd2ba2e1ec8a0a
Daniil Gavrilov <daniil.gavrilov.10@gmail.com>
2025-03-19 15:11:16 +0400

    add generate()
    # Conflicts:
    #	login.py

--- login.py
+++ login.py
@@ -1,5 +1,8 @@
 user = input("INPUT USER NAME > ")
 password = input("INPUT PASSWORD > ")
 
-print("YOU LOGIN")
-print(f"Your username: {user}", f"Your password: {password}", sep="\n")
+print("YOU LOGIN") # from master
+def generate(info):
+    return "".join(map(lambda char: f"+{char}+",info))
+
+print(f"Your username: {generate(user)}", f"Your password: {generate(password)}", sep="\n")
